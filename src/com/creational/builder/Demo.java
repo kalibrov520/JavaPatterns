b71676deaa3203095978b4fba66471245e6d5ece
kalibrov520 <kalibrov520@gmail.com>
2020-05-12 14:11:03 +0300

    Builder demo fix

--- src/com/creational/builder/Demo.java
+++ src/com/creational/builder/Demo.java
@@ -4,20 +4,6 @@ import com.creational.builder.utility.*;
 
 public class Demo {
     public static void main(String[] args) {
-//        Director director = new Director();
-//
-//        CarBuilder builder = new CarBuilder();
-//        director.constructSportsCar(builder);
-//
-//        Car car = builder.build();
-//        System.out.println("Car built:\n" + car.getType());
-//
-//        CarManualBuilder manualBuilder = new CarManualBuilder();
-//        director.constructSportsCar(manualBuilder);
-//
-//        Manual carManual = manualBuilder.getResult();
-//        System.out.println("\nCar manual built:\n" + carManual.toString());
-
         Car car = new CarBuilder().setType(Type.SPORTS_CAR)
                 .setSeats(2)
                 .setEngine(new Engine(2.0, 4.0))
@@ -30,6 +16,5 @@ public class Demo {
                 .setTransmission(Transmission.SEMI_AUTOMATIC).build();
 
         System.out.println(manual);
-
     }
 }
