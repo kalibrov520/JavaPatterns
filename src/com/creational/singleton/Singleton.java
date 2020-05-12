192137a542b16fa5ef17718ee054660a873fa7be
kalibrov520 <kalibrov520@gmail.com>
2020-05-12 15:11:24 +0300

    Double Checked Locking & volatile Singleton

---
+++ src/com/creational/singleton/Singleton.java
@@ -0,0 +1,18 @@
+package com.creational.singleton;
+
+public class Singleton {
+    private static volatile Singleton instance;
+
+    public static Singleton getInstance() {
+        Singleton localInstance = instance;
+        if (localInstance == null) {
+            synchronized (Singleton.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new Singleton();
+                }
+            }
+        }
+        return localInstance;
+    }
+}
