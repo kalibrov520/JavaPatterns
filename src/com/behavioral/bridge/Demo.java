cf90b0956e743d4cf559f729b937da06f19ca79a
kalibrov520 <kalibrov520@gmail.com>
2020-05-14 23:09:09 +0300

    Bridge pattern

---
+++ src/com/behavioral/bridge/Demo.java
@@ -0,0 +1,27 @@
+package com.behavioral.bridge;
+
+import com.behavioral.bridge.devices.Device;
+import com.behavioral.bridge.devices.Radio;
+import com.behavioral.bridge.devices.Tv;
+import com.behavioral.bridge.remotes.AdvancedRemote;
+import com.behavioral.bridge.remotes.BasicRemote;
+
+public class Demo {
+    public static void main(String[] args) {
+        testDevice(new Tv());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+    }
+}
---
+++ src/com/behavioral/bridge/remotes/Remote.java
@@ -0,0 +1,13 @@
+package com.behavioral.bridge.remotes;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
